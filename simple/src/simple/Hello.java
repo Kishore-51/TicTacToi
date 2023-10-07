cf15d712b82ceba856b5b746df196b1641428f30
Kishore@123 <thedeadchest@gmail.com>
2023-10-07 21:39:19 +0530

    First commit

---
+++ simple/src/simple/Hello.java
@@ -0,0 +1,12 @@
+package simple;
+import java.util.*;
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO Kishore");
+		System.out.println("HELLO Kishore");
+
+	}
+
+}
